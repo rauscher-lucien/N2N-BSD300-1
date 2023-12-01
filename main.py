684057800a7fb3fa8c424a70a2ba6eccdd186f8b
rauscher-lucien <rauscher.lucien@gmail.com>
2023-12-01 10:49:03 +0100

    minor changes

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     data_dict['beta2'] = 0.999
 
     # hyperparameters
-    data_dict['num_epochs'] = 100
+    data_dict['num_epochs'] = 300
     data_dict['epoch_save_freq'] = 10
     data_dict['epoch_to_load'] = None
 
